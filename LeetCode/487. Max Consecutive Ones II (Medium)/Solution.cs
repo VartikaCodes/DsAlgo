65cd708377e7a82c7e0fff1d8274425403a51eee
Vartika Priyadarshini <magicalvartika06@gmail.com>
2022-10-31 23:50:00 +0530

    487. Max Consecutive Ones II

---
+++ LeetCode/487. Max Consecutive Ones II (Medium)/Solution.cs
@@ -0,0 +1,39 @@
+public class Solution
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        var ans = 1; // Expected Value
+        var j = 0; // Start of first window
+        var k = 1; // No of flips allowed
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            /*If item does not match the expected value
+             * 1.Flip it, to make it part of the current window
+             *2.Decrease the number of flips available
+             */
+            if (nums[i] != ans)
+            {
+                k--;
+            }
+
+            // If no more flips available, slide the window.
+            if (k < 0)
+            {
+                /* If the item that required a flip, is slided over
+                 * and is not part of the current window, anymore
+                 * reset the flip count.
+                */
+                if (nums[j++] != ans)
+                {
+                    k++;
+                }
+            }
+        }
+
+        /* Return the length of the array minus the start of the largest window.
+         * This will be the max. length of the consecutive occurances of that char.
+        */
+        return nums.Length - j;
+    }
+}
